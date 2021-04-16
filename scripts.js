8c0f7ca969f8395bb7b48228d1aa79fecb305560
frol <50583754+frol2256@users.noreply.github.com>
2021-04-16 07:39:18 +0100

    Update scripts.js

---
+++ scripts.js
@@ -0,0 +1,40 @@
+function scrollToTop() {
+    window.scrollTo(0, -10);
+}
+
+function DarkMode() {
+
+    var elem = document.getElementById("DarkModeButton");
+    if (elem.innerHTML == "Dark Mode") {
+        elem.innerHTML = "Light Mode";
+
+        elem.style.color = "#FFFFFF";
+        elem.style.backgroundColor = "#1C1C1C";
+        elem.style.borderColor = "#FFFFFF";
+
+        document.body.style.backgroundColor = "#1C1C1C";
+        document.body.style.color = "#FFFFFF";
+        document.getElementById("heading").style.color = "#f6b93b";
+        var ilist = document.images;
+
+        for (var i = 0; i < ilist.length; i++) {
+            ilist[i].style.borderColor = 'white';
+        }
+    }
+
+    else {
+        elem.innerHTML = "Dark Mode";
+
+        document.body.style.backgroundColor = "#FFFFFF";
+        document.body.style.color = "#1C1C1C";
+        elem.style.backgroundColor = "#FFFFFF";
+        elem.style.color = "#1C1C1C";
+        elem.style.borderColor = "#1C1C1C";
+        document.getElementById("heading").style.color = "#1C1C1C";
+        var ilist = document.images;
+
+        for (var i = 0; i < ilist.length; i++) {
+            ilist[i].style.borderColor = '#333333';
+        }
+    }
+}
